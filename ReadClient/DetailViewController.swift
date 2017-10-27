38e8abb5bce3d70c1266ba4bd9030bb8e24b35b4
talisk <talisk@talisk.cn>
2017-10-27 16:53:15 +0800

    Scrollview hide

--- ReadClient/DetailViewController.swift
+++ ReadClient/DetailViewController.swift
@@ -34,17 +34,37 @@ class DetailViewController: UIViewController {
     func setupNavigationBar() {
         let leftItem = UIBarButtonItem(title: "返回", style: UIBarButtonItemStyle.plain, target: self, action: #selector(pop))
         self.navigationItem.leftBarButtonItem = leftItem
+        
+        let rightItem = UIBarButtonItem(title: "测试", style: UIBarButtonItemStyle.plain, target: self, action: #selector(showAllSubView))
+        self.navigationItem.rightBarButtonItem = rightItem
     }
     
     func setupScrollView() {
         self.scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: Constant.ScreenWidth, height: Constant.ScreenHeight))
         self.scrollView.backgroundColor = UIColor.white
+        self.scrollView.contentSize = CGSize(width: Constant.ScreenWidth, height: 4000)
+        self.scrollView.delegate = self
         self.view.addSubview(self.scrollView)
     }
 }
 
-extension DetailViewController {
+extension DetailViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        for replicantView in self.view.subviews {
+            if replicantView.frame.height == 300 {
+                replicantView.isHidden = true
+            }
+        }
+    }
     
+    func showAllSubView() {
+        print(self.view.subviews.debugDescription)
+        for replicantView in self.view.subviews {
+            if replicantView.frame.height == 300 {
+                replicantView.isHidden = true
+            }
+        }
+    }
 }
 
 extension DetailViewController {
